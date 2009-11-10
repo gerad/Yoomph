f032e7e7318a0c6918acff737fd8be6f810af457
Gerad Suyderhoud <gerad@gecs.local>
2009-11-10 08:23:36 -0800

    working on building my own class object

--- app/models/person.js
+++ app/models/person.js
@@ -1,10 +1,4 @@
 new function(_) { // closure
-  var PersonPackage = new base2.Package(this, {
-    name: "PersonPackage",
-    version: "0.1",
-    imports: "KitePackage",
-    exports: "Person"
-  });
   eval(this.imports);
 
   var Person = Model.extend({
