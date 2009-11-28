ff1c8c37609516a74b9c72b8463107dea1517598
Gerad Suyderhoud <gerad@gecs.local>
2009-11-28 11:10:04 -0800

    allow mocking of load

---
+++ test/helper.js
@@ -0,0 +1,18 @@
+new function(_) {
+eval(Kite.provides());
+
+/* load mocking */
+var load = provide.load, loadMock = null;
+provide.load = function() {
+	return loadMock ?
+		loadMock.apply(null, arguments):
+		load.apply(null, arguments);
+};
+provide.load.mock = function(mocked) {
+	loadMock = mocked;
+};
+provide.load.reset = function() {
+	loadMock = null;
+};
+
+}
